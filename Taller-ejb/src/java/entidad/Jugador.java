1a012e159310e6d391442d935ab7c310db41cda6
martin <martin@MartinE>
2015-06-11 18:53:43 -0300

    Merge origin/master
    Conflicts:
    	Taller-ejb/src/java/entidad/Equipo.java

--- Taller-ejb/src/java/entidad/Jugador.java
+++ Taller-ejb/src/java/entidad/Jugador.java
@@ -6,9 +6,6 @@ import java.util.Date;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
@@ -20,11 +17,6 @@ import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 public class Jugador extends Usuario implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-          
     @ManyToMany(cascade = CascadeType.ALL) 
     @JoinTable(name="jugador_equipo", 
             joinColumns = @JoinColumn(name="jugador_id", 
@@ -42,20 +34,11 @@ public class Jugador extends Usuario implements Serializable {
 
     public Jugador(Long id, String nombre, Date fechaNacimiento, List<Equipo> equipos, Integer telefono,Integer puntuacion,
             String email,String contrasenia,String direccion) {
-        super(telefono,nombre,email,contrasenia,direccion,fechaNacimiento);
-        this.id = id;
+        super(id,telefono,nombre,email,contrasenia,direccion,fechaNacimiento);
         this.equipos = equipos;
         this.puntuacion = puntuacion;
     }
-    
-    public Long getId() {
-        return id;
-    }
-    
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public List<Equipo> getEquipos() {
         return equipos;
@@ -72,32 +55,6 @@ public class Jugador extends Usuario implements Serializable {
     public void setPuntuacion(Integer puntuacion) {
         this.puntuacion = puntuacion;
     }
-        
-    
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Jugador)) {
-            return false;
-        }
-        Jugador other = (Jugador) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "entidad.Jugador[ id=" + id + " ]";
-    }
+     
     
 }
